f8d08f16a2c7829e82e744118191734ff9534f81
Renatho <renathohcc@hotmail.com>
2025-04-30 14:54:28 -0300

    Ajuste texto na pipeline python

--- Resources/etl_pipeline.py
+++ Resources/etl_pipeline.py
@@ -1,29 +1,29 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Carregar o arquivo CSV
+# Load the CSV File
 file_path = 'thyroid_cancer_risk_data.csv'
 df = pd.read_csv(file_path)
 
-# Tratamentos dos dados
-# 1. Remover duplicatas
+
+# 1. Remove doubles
 print(f"Linhas antes da remoção de duplicatas: {len(df)}")
 df = df.drop_duplicates(subset='Patient_ID')
 print(f"Linhas após a remoção de duplicatas: {len(df)}")
 
-# 2. Padronizar textos
+# 2. Standardize texts
 cols_to_strip = ['Gender', 'Country', 'Ethnicity', 'Family_History', 'Radiation_Exposure',
                  'Iodine_Deficiency', 'Smoking', 'Obesity', 'Diabetes', 'Thyroid_Cancer_Risk', 'Diagnosis']
 df[cols_to_strip] = df[cols_to_strip].apply(lambda x: x.str.strip().str.lower())
 
-# 3. Converter Yes/No para Y/N
+# 3. Convert Yes/No to Y/N
 yes_no_cols = ['Family_History', 'Radiation_Exposure', 'Iodine_Deficiency', 'Smoking', 'Obesity', 'Diabetes']
 df[yes_no_cols] = df[yes_no_cols].replace({'yes': 'y', 'no': 'n'})
 
-# 4. Verificar valores ausentes
+# 4. Verify missing values
 df.fillna({'TSH_Level': 0.0, 'T3_Level': 0.0, 'T4_Level': 0.0, 'Nodule_Size': 0.0}, inplace=True)
 
-# 5. Configurar tipos de dados
+# 5. Setup data types
 df = df.astype({
     'Patient_ID': 'int64',
     'Age': 'int64',
@@ -33,14 +33,14 @@ df = df.astype({
     'Nodule_Size': 'float64'
 })
 
-# Conexão com o banco MySQL
+# Connect to MySQL
 user = 'root'
 password = 'hopt1234'
 host = 'localhost'
 database = 'thyroid'
 engine = create_engine(f'mysql+pymysql://{user}:{password}@{host}/{database}')
 
-# Importar os dados para o MySQL
+# Import data to MySQL
 df.to_sql('thyroid_cancer_data', con=engine, if_exists='replace', index=False)
 
 print("Dados importados com sucesso!")
